e8c8f699ea360207928c2dc9e760ffb03878fa35
Dhanusri-26 <s.dhanusri2005@gmail.com>
2025-05-15 09:38:17 +0530

    Add files via upload

---
+++ day1.c/celsius.c
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    double celciusToFahrenheit(int C) {
+        // code here
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout <<setprecision(2)<<fixed<< ob.celciusToFahrenheit(N) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0; 
+}
